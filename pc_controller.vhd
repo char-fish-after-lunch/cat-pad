b32ebd5dfdb41d68221a0ae8f75043a94a98f217
maoyuchaxue <wangjt15@mails.tsinghua.edu.cn>
2017-11-17 15:18:27 +0800

    pc controller added

---
+++ pc_controller.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:17:51 11/17/2017 
+-- Design Name: 
+-- Module Name:    pc_controller - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity pc_controller is
+end pc_controller;
+
+architecture Behavioral of pc_controller is
+
+begin
+
+
+end Behavioral;
+
