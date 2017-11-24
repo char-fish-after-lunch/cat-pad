56039e7a4b211aeb57618be83f78ca1b176881f1
Johnmmm <945841612@qq.com>
2017-11-24 21:34:45 +0800

    unit test in fu finished and I think it is right

--- forward_unit.vhd
+++ forward_unit.vhd
@@ -38,8 +38,7 @@ entity forward_unit is port(
 		ramRead		: in std_logic;
 		oprSrcB		: in std_logic;
 		--add one to check if it is reading from ram
-		
-		help	: out std_logic_vector(3 downto 0);
+
 		srcA	: out std_logic_vector(1 downto 0);
 		srcB	: out std_logic_vector(1 downto 0)
 	);
@@ -50,7 +49,6 @@ architecture Behavioral of forward_unit is
 begin
 	process(regReadSrcA, regReadSrcB, memDst, wbDst, ramRead, oprSrcB)
 	begin
-	help <= regReadSrcB;
 	if(regReadSrcA = memDst and regReadSrcA = wbDst and ramRead = '0') then
 		srcA <= fwd_wb_ram;
 		--both need to write, use mem
@@ -72,13 +70,11 @@ begin
 		srcB <= fwd_wb_ram;
 		--both need to write, use mem
 	elsif(regReadSrcB = memDst and ramRead = '0') then
-		srcB <= "11";
+		srcB <= fwd_alu_res;
 		--use data from mem
 	elsif(regReadSrcB = wbDst and ramRead = '0') then
 		srcB <= fwd_wb_alu;
 		--use data from wb
-	elsif(regReadSrcB = "0011") then
-		srcB <= "11";
 	else
 		srcB <= fwd_original;
 		--else use register or imm
