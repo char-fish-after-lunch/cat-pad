aa39d9bcdaa308f108c5f93de386033ed1271e7c
Johnmmm <945841612@qq.com>
2017-11-28 00:30:43 +0800

    没加使能的看起来是对的的带显卡的vga

--- vga_control.vhd
+++ vga_control.vhd
@@ -34,12 +34,23 @@ use ieee.std_logic_arith.all;
 entity vga_control is port(
         clk_in : in std_logic;
         rst : in std_logic;
+        ramWrite : in std_logic;
 
         out_red : out std_logic_vector(2 downto 0);
         out_green : out std_logic_vector(2 downto 0);
         out_blue : out std_logic_vector(2 downto 0);
 
-        test : out std_logic_vector(2 downto 0);
+        romAddr : out std_logic_vector(17 downto 0);
+        romData : in std_logic_vector(15 downto 0); --rgb that only 8 downto 0 is useful
+        vgaRead : out std_logic;
+
+        ramWriAddrIn : in std_logic_vector(17 downto 0); --video card function 
+        ramWriDataIn : in std_logic_vector(15 downto 0); --in
+        ramWriAddrOut : out std_logic_vector(17 downto 0); --record it for a while and then 
+        ramWriDataOut : out std_logic_vector(15 downto 0); --send it to the ram
+
+        save : out std_logic;
+
         hs : out std_logic;
         vs : out std_logic
     );
@@ -47,21 +58,27 @@ end vga_control;
 
 architecture Behavioral of vga_control is
     -- some tmp signal
-    signal clk, clk_2 : std_logic := '0';
-    signal r_t, g_t, b_t, test_t : std_logic_vector (2 downto 0);
+    signal clk, clk_60 : std_logic := '0';
+    signal r_t, g_t, b_t : std_logic_vector (2 downto 0);
     signal hs_t,vs_t : std_logic;
     signal x, y : integer range 0 to 1000;
+    signal readData : std_logic;
+    signal tmp_Addr : std_logic_vector(17 downto 0); --record those addr and data for a while
+    signal tmp_Data : std_logic_vector(15 downto 0);
+    signal something_save : std_logic := '0';
 
 begin
 
-    process (clk_in) -- make that 25Hz
+    process (clk_in) -- make that 25MHz
 	begin
 		if clk_in'event and clk_in = '1' then
 			clk <= not clk;
 		end if;
     end process;
+
+    --VGA functions:
     
-    process(clk, rst, x) --������ɨ��
+    process(clk, rst, x) --800x525
     begin
         if rst = '0' then
 			x <= 0;
@@ -74,7 +91,7 @@ begin
 		end if;
     end process;
 
-    process(clk, rst, y, x) --������ɨ��
+    process(clk, rst, y, x) --800X525
     begin
         if rst = '0' then
 			y <= 0; 
@@ -89,7 +106,7 @@ begin
         end if;
     end process;
 
-    process(clk, rst, x) --������ͬ���ź�
+    process(clk, rst, x) --656~752
     begin
         if rst = '0' then
             hs_t <= '1';
@@ -102,7 +119,7 @@ begin
         end if;
     end process;
 
-    process(clk, rst, y) --���ó�ͬ���ź�
+    process(clk, rst, y) --490~492
     begin
         if rst = '0' then
             vs_t <= '1';
@@ -115,8 +132,8 @@ begin
         end if;
     end process;
 
-    process(clk, rst, hs_t) --���ź����
-    begin
+    process(clk, rst, hs_t) --for hs
+	 begin
         if rst = '0' then
             hs <= '0';
         elsif clk'event and clk = '1' then
@@ -124,52 +141,49 @@ begin
         end if;
     end process;
 
-    process(clk, rst, vs_t) --���ź����
-    begin
+    process(clk, rst, vs_t) --for vs
+	begin
         if rst = '0' then
             vs <= '0';
         elsif clk'event and clk = '1' then
             vs <= vs_t;
         end if;
     end process;
+
+    process(clk, rst)
+    begin
+        if rst = '0' then
+            vgaRead <= '0';
+            readData <= '0';
+        elsif clk'event and clk = '1' then
+            vgaRead <= '1';
+            readData <= '1';
+        elsif clk'event and clk = '0' then
+            vgaRead <= '0';
+            readData <= '0';
+        end if;
+    end process;
     
-    process(clk, rst, x, y)
+    process(clk, rst, x, y) --control the rgb
     begin
         if rst = '0' then
             r_t <= "000";
             g_t <= "000";
             b_t <= "000";
-            test_t <= "000";
         elsif clk'event and clk = '1' then
-            if x > 0 and x < 20 and y > 0 and y < 20 then
-                r_t <= "111";
-                g_t <= "111";
-                b_t <= "111";
-			elsif x > 620 and x < 640 and y > 0 and y < 20 then
-				r_t <= "111";
-                g_t <= "000";
-                b_t <= "000";
-			elsif x > 620 and x < 640 and y > 460 and y < 480 then
-				r_t <= "000";
-                g_t <= "111";
-                b_t <= "000";
-            elsif x > 0 and x < 20 and y > 460 and y < 480 then
-                r_t <= "000";
-                g_t <= "000";
-                b_t <= "111";
-            elsif x > 310 and x < 330 and y > 0 and y < 480 then
-                r_t <= "111";
-                g_t <= "111";
-                b_t <= "111";
-            elsif x > 80 and x < 120 and y > 40 and y < 80 then
-                r_t <= "111";
-                g_t <= "111";
-                b_t <= "111";
-            elsif x > 0 and x < 640 and y > 0 and y < 480 then
-                r_t <= "000";
-                g_t <= "111";
-                b_t <= "000";
-                test_t <= "100";
+            if x >= 0 and x < 640 and y >= 0 and y < 480 then
+                if x >= 64 and x < 576 and y >= 0 and y < 480 then
+                    romAddr(17 downto 9) <= conv_std_logic_vector(x-64, 9);
+                    romAddr(8 downto 0) <= conv_std_logic_vector(y, 9);
+                    r_t <= romData(8 downto 6);
+                    g_t <= romData(5 downto 3);
+                    b_t <= romData(2 downto 0);
+                else
+                    romAddr(17 downto 0) <= (others => '0');
+                    r_t <= (others => '0');
+                    g_t <= (others => '0');
+                    b_t <= (others => '0');
+                end if;
             else
                 r_t <= (others => '0');
                 g_t <= (others => '0');
@@ -178,19 +192,66 @@ begin
         end if;
     end process;
 
-    process(hs_t, vs_t, r_t, g_t, b_t)
+    process(hs_t, vs_t, r_t, g_t, b_t) --put them into output
     begin 
         if hs_t = '1' and vs_t = '1' then
 			out_red <= r_t;
 			out_green <= g_t;
 			out_blue <= b_t;
-			test <= test_t; --it is always this, why???
 		else
 			out_red <= (others => '0');
 			out_green <= (others => '0');
 			out_blue <= (others => '0');
-			test <= test_t;
 		end if;
+    end process;
+
+    --video card functions:
+
+    process(clk, rst, ramWriAddrIn, ramWriDataIn, ramWrite, readData, something_save) --when some data
+    begin
+        if rst = '0' then
+            --shi neng wei ling
+            ramWriAddrOut <= (others => '0');
+            ramWriDataOut <= (others => '0');
+            tmp_Addr <= (others => '0');
+            tmp_Data <= (others => '0');
+            something_save <= '0';
+        elsif clk'event and clk = '1' then  --if the state is read
+            --if readData = '1' then 
+            -- shi neng zhi ling
+            ramWriAddrOut <= (others => '0');
+            ramWriDataOut <= (others => '0');
+            if ramWrite = '1' then --if need to write, then save it
+                tmp_Addr <= ramWriAddrIn;
+                tmp_Data <= ramWriDataIn;
+                something_save <= '1';
+            end if;
+        elsif clk'event and clk = '0' then -- if the state is write
+            if ramWrite = '1' and something_save = '0' then
+                -- shineng!!
+                ramWriAddrOut <= ramWriAddrIn;
+                ramWriDataOut <= ramWriDataIn;
+            elsif ramWrite = '1' and something_save = '1' then
+                -- shineng!!
+                ramWriAddrOut <= tmp_Addr;
+                ramWriDataOut <= tmp_Data;
+                tmp_Addr <= ramWriAddrIn;
+                tmp_Data <= ramWriDataIn;
+                something_save <= '1';
+            elsif ramWrite = '0' and something_save = '1' then
+                -- shineng!!
+                ramWriAddrOut <= tmp_Addr;
+                ramWriDataOut <= tmp_Data;
+                tmp_Addr <= (others => '0');
+                tmp_Data <= (others => '0');
+                something_save <= '0';
+            else
+                --shi neng zhi ling
+                ramWriAddrOut <= (others => '0');
+                ramWriDataOut <= (others => '0');
+            end if;
+        end if;
+        save <= something_save;
     end process;
 
 end Behavioral;
