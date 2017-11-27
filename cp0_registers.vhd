d628f24b376b9f5f6767ecd44c020e5e28bd5f61
YU_Jason <yuzhijingcheng1996@hotmail.com>
2017-11-27 11:46:24 +0800

    #8 A stupid CP0 register file added. A stupid interrupt control (aka CP0) partly implemented.

--- cp0_registers.vhd
+++ cp0_registers.vhd
@@ -4,15 +4,32 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity cp0_registers is
 	port(
-		cause: in std_logic_vector(7 downto 0);
-		epc: in std_logic_vector(15 downto 0)
+		clk : in std_logic;
+		causeIn : in std_logic_vector(15 downto 0);
+		epcIn : in std_logic_vector(15 downto 0);
+		statusIn : in std_logic;
+		trapIn : in std_logic;
+		eret : in std_logic;
 
-
-		
-		cause_o: out std_logic;
-		epc_o: out std_logic;
-		status_o: out std_logic
+		cause : out std_logic_vector(15 downto 0);
+		epc : out std_logic_vector(15 downto 0);
+		status :  out std_logic_vector;
+		trap : out std_logic_vector;
+		eret : out std_logic_vector
 	);
-	
 end entity;
+
+architecture Behavioral of cp0_registers is
+begin
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			cause <= causeIn;
+			epc <= epcIn;
+			status <= statusIn;
+			eret <= eretIn;
+			trap <= trapIn;
+		end if;
+	end process;
+end Behavioral;
 
--- interrupt_control.vhd
+++ interrupt_control.vhd
@@ -5,37 +5,73 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity interrupt_control is
 	port(
 		wbInt: in std_logic; -- whether there is an interrupt in WB
-		wbIntCode: in std_logic_vector(7 downto 0) -- interrupt code in WB
+		wbIntCode: in std_logic_vector(3 downto 0); -- interrupt code in WB
+		wbERet : in std_logic; -- whether the instruction is an eret
+		memAddress : in std_logic_vector(15 downto 0); -- the address of the next instruction
+
+		cp0Status : in std_logic;
+		cp0Epc : in std_logic_vector(15 downto 0);
+		cp0Cause : in std_logic_vector(15 downto 0);
+
+		-- external hardware ISRs
+		ps2Request : in std_logic; -- PS/2 ISR
 		
 		memRamLock: out std_logic;
 		pipelineClear: out std_logic; -- whether clear the whole pipeline
 
-				
-		
-		-- force the PC to ...
-		setPC: out std_logic;
-		setPCVal: out std_logic_vector(15 downto 0); 
+		cp0StatusUpdate : out std_logic;
+		cp0EpcUpdate : out std_logic_vector(15 downto 0);
+		cp0CauseUpdate : out std_logic_vector(15 downto 0)
+		cp0ERetUpdate : out std_logic_vector;
+		cp0TrapUpdate : out std_logic_vector
 	);
 end interrupt_control;
 
 architecture behavioral of interrupt_control is
 begin
-	process(wbInt, wbIntCode)
+	process(wbInt, wbIntCode, cp0Cause, cp0Epc, cp0Status, memAddress)
+		variable cp0Changed : std_logic := '0';
+		variable cp0CauseUpdateV : std_logic_vector(15 downto 0) := cp0Cause;
+		variable cp0EpcUpdateV : std_logic_vector(15 downto 0) := cp0Epc;
+		variable cp0StatusUpdateV : std_logic := cp0Status;	
 	begin
 		memRamLock <= '0';
 		pipelineClear <= '0';
-		setPC <= '0';
+		cp0ERetUpdate <= '0';
+		cp0TrapUpdate <= '0';
 
-		if wbInt then
+		if ps2Request = '1' then
+			cp0Changed := '1';
+			cp0CauseUpdateV(11) := '1' -- 11 for PS/2 ISR
+		end if;
+
+		if wbInt = '1' then
 			-- an interrupt has happened!
 			-- keep the address of the current address and the next address
 			-- and prevents the next instruction from writing RAM (for it is now in the MEM stage)
+
+			if cp0Status = '0' then -- if interrupt allowed
+				cp0Changed := '1';
+				cp0CauseUpdateV(to_unsigned(wbIntCode)) := '1'; 
+			end if;
+			
+		elsif wbERet = '1' then
+			cp0ERetUpdate <= '1';	
+			-- we assume that eret and int cannot be 1 at the same time
+		end if;
+		if cp0Changed = '1' and cp0Status = '0' then
+			-- this means that the CP0 running state would be changed (switched
+			-- from the user mode to the kernel mode)
 			memRamLock <= '1';
 			pipelineClear <= '1';
-
-			setPC <= '1';
-			setPCVal <= (15 downto 4 => '0') & "101"; -- set PC to 0x0005, the start of the interrupt handler
+			cp0EpcUpdateV := memAddress;
+			cp0TrapUpdate <= '1';
 		end if;
+	
+		cp0StatusUpdate <= cp0CauseUpdateV;
+		cp0EpcUpdate <= cp0EpcUpdateV;
+		cp0CauseUpdate <= cp0StatusUpdateV;
+
 	end process;
 end behavioral;
 
