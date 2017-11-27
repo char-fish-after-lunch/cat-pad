f591fb918409a9503bcaee29c468572cbf2ba6c9
YU_Jason <yuzhijingcheng1996@hotmail.com>
2017-11-27 12:17:31 +0800

    #9 Interrupt controller updated. Nearly finished.

--- interrupt_control.vhd
+++ interrupt_control.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 
 entity interrupt_control is
@@ -12,6 +13,8 @@ entity interrupt_control is
 		cp0Status : in std_logic;
 		cp0Epc : in std_logic_vector(15 downto 0);
 		cp0Cause : in std_logic_vector(15 downto 0);
+		cp0ERet : in std_logic_vector;
+		cp0Trap : in std_logic_vector;
 
 		-- external hardware ISRs
 		ps2Request : in std_logic; -- PS/2 ISR
@@ -23,26 +26,34 @@ entity interrupt_control is
 		cp0EpcUpdate : out std_logic_vector(15 downto 0);
 		cp0CauseUpdate : out std_logic_vector(15 downto 0)
 		cp0ERetUpdate : out std_logic_vector;
-		cp0TrapUpdate : out std_logic_vector
+		cp0TrapUpdate : out std_logic_vector;
+
+		pcSet : out std_logic;
+		pcSetVal : out std_logic_vector(15 downto 0)
 	);
 end interrupt_control;
 
 architecture behavioral of interrupt_control is
 begin
 	process(wbInt, wbIntCode, cp0Cause, cp0Epc, cp0Status, memAddress)
-		variable cp0Changed : std_logic := '0';
+		type Cause is range 0 to 10;
+
+		variable cp0IntEvent : std_logic := '0';
 		variable cp0CauseUpdateV : std_logic_vector(15 downto 0) := cp0Cause;
 		variable cp0EpcUpdateV : std_logic_vector(15 downto 0) := cp0Epc;
 		variable cp0StatusUpdateV : std_logic := cp0Status;	
+		variable cp0NextCause : Cause := 0;
 	begin
 		memRamLock <= '0';
 		pipelineClear <= '0';
 		cp0ERetUpdate <= '0';
 		cp0TrapUpdate <= '0';
+		pcSet <= '0';
+
 
 		if ps2Request = '1' then
-			cp0Changed := '1';
-			cp0CauseUpdateV(11) := '1' -- 11 for PS/2 ISR
+			cp0IntEvent := '1';
+			cp0CauseUpdateV(10) := '1' -- 11 for PS/2 ISR
 		end if;
 
 		if wbInt = '1' then
@@ -51,7 +62,7 @@ begin
 			-- and prevents the next instruction from writing RAM (for it is now in the MEM stage)
 
 			if cp0Status = '0' then -- if interrupt allowed
-				cp0Changed := '1';
+				cp0IntEvent := '1';
 				cp0CauseUpdateV(to_unsigned(wbIntCode)) := '1'; 
 			end if;
 			
@@ -59,7 +70,48 @@ begin
 			cp0ERetUpdate <= '1';	
 			-- we assume that eret and int cannot be 1 at the same time
 		end if;
-		if cp0Changed = '1' and cp0Status = '0' then
+
+		if cp0ERet = '1' then
+			-- switch to user mode
+			
+			if cp0CauseUpdateV(11 downto 0) = (10 downto 0 => '0') then
+
+				cp0StatusUpdateV := '0'; -- allow interrupt
+				pcSet <= '1';
+				pcSetVal <= cp0Epc;
+			else
+				-- pending interrupts exist
+				-- go back to the interrupt handler
+				for i in Cause loop
+					if cp0CauseUpdateV(i) = '1' then 
+						cp0NextCause := i;
+					end if;
+				end loop;
+				cp0CauseUpdateV(cp0NextCause) := '0';
+				cp0CauseUpdateV(14 downto 11) := std_logic_vector(to_unsigned(cp0NextCause, 4));
+
+				pcSet <= '1';
+				pcSetVal <= (15 downto 3 => '0') & "101";
+			end if;
+		elsif cp0Trap = '1' then
+			-- switch to kernel mode
+			for i in Cause loop
+				if cp0CauseUpdateV(i) = '1' then 
+					cp0NextCause := i;
+				end if;
+			end loop;
+			cp0CauseUpdateV(cp0NextCause) := '0';
+			cp0CauseUpdateV(14 downto 11) := std_logic_vector(to_unsigned(cp0NextCause, 4));
+
+			pcSet <= '1';
+			pcSetVal <= (15 downto 3 => '0') & "101";
+
+			cp0StatusUpdateV := '1';
+		end if;
+
+
+
+		if cp0IntEvent = '1' and cp0StatusUpdateV = '0' then
 			-- this means that the CP0 running state would be changed (switched
 			-- from the user mode to the kernel mode)
 			memRamLock <= '1';
